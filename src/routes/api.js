d470fe51df113032460803f1a1113a199b624306
Rick Hitchcock <rickhitchcock@outlook.com>
2026-03-20 13:04:34 -0400

    diagnostics

--- src/routes/api.js
+++ src/routes/api.js
@@ -16,27 +16,33 @@ const postIssue = async ({
   comments = '',
   labels = [],
 }) => {
-  if (!client) {
-    const app = new Octokit({
-      authStrategy: createAppAuth,
-      auth: { appId, privateKey },
-    });
+  try {
+    if (!client) {
+      const app = new Octokit({
+        authStrategy: createAppAuth,
+        auth: { appId, privateKey },
+      });
 
-    const { data } = await app.request('GET /orgs/{org}/installation', { org: organization });
+      const { data } = await app.request('GET /orgs/{org}/installation', { org: organization });
 
-    client = new Octokit({
-      authStrategy: createAppAuth,
-      auth: { appId, privateKey, installationId: data.id },
+      client = new Octokit({
+        authStrategy: createAppAuth,
+        auth: { appId, privateKey, installationId: data.id },
+      });
+    }
+
+    return client.request('POST /repos/{owner}/{repo}/issues', {
+      owner: organization,
+      repo: title === 'CRASH' ? 'dst-crash' : repository,
+      title,
+      body: `\`\`\`\n${name}\n${email}\n${comments.replace(/^\s+/gm, '')}`.trim(),
+      labels,
     });
+  } catch (error) {
+    return {
+      error,
+    };
   }
-
-  return client.request('POST /repos/{owner}/{repo}/issues', {
-    owner: organization,
-    repo: title === 'CRASH' ? 'dst-crash' : repository,
-    title,
-    body: `\`\`\`\n${name}\n${email}\n${comments.replace(/^\s+/gm, '')}`.trim(),
-    labels,
-  });
 }; // postIssue
 
 export default async function apiRoutes(app) {
@@ -47,7 +53,6 @@ export default async function apiRoutes(app) {
     'Database',
     'Create an Issue Object, which posts to GitHub',
     async (repository, title, name, email, comments, labels) => {
-      postIssue({ repository, title, name, email, comments, labels });
       const date = new Date();
       pool.query(
         `
@@ -66,6 +71,11 @@ export default async function apiRoutes(app) {
           date,
         ],
       );
+
+      const github = await postIssue({ repository, title, name, email, comments, labels });
+      if (github.error) {
+        return github;
+      }
 
       return {
         data: { status: 'success' },
